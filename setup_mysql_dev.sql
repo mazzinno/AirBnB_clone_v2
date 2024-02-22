f96cadb420f69738f680963154e44093d2e64f65
Oycctem <ousstemm@gmail.com>
2024-02-21 16:42:04 -0800

    done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
--- prepares a MySQL server for 
+-- Creates a new database named 'hbnb_dev_db' if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,6 @@
--- prepares a MySQL server for the project
-
+-- Check if the 'hbnb_test_db' database exists, and create it if it doesn't
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
