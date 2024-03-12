2c0acd9b75cb7a0779272d208ae4bfb086064e1d
mazzinno <abdennasr3@gmail.com>
2024-03-12 02:53:01 +0000

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from web static folder."""
-from fabric.api import *
-from time import strftime
-from datetime import date
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
-    """
-    script that generates a .tgz archive  of
-    the contents of the web_static folder.
-    """
-    file_name = strftime("%Y%m%d%H%M%S")
+    """generates a tgz archive"""
     try:
-        local("mkdir -p versions")
-        local(f"tar -czvf versions/web_static_{file_name}.tgz web_static/")
-        return f"versions/web_static_{file_name}.tgz"
-
-    except Exception as e:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
         return None
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,57 +1,47 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
 from fabric.api import *
+from os.path import exists
 from datetime import datetime
-from os import path
-
+from fabric.api import local
 
 env.hosts = ['100.26.237.194', '34.239.255.112']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+
+
+def do_pack():
+    '''
+    Fabric script that generates a .tgz archive from the
+    contents of the web_static
+    '''
+    try:
+        filepath = 'versions/web_static_' + datetime.now().\
+                   strftime('%Y%m%d%H%M%S') + '.tgz'
+        local('mkdir -p versions')
+        local('tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+        web_static')
+        print('web_static packed: {} -> {}'.
+              format(filepath, os.path.getsize(filepath)))
+    except Exception as e:
+        return None
 
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
-
-                # upload archive
-                put(archive_path, '/tmp/')
-
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
-
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-                # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-                # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                    .format(timestamp))
-
-                # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
-
-                # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
-
-        # return True on success
+    """Depploy to yoru webs server"""
+    if exists(archive_path) is False:
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases'
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}'.format(file_path, file_name[:-4]))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                               file_path, file_name[:-4]))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}{}/web_static/* {}{}/'.format(file_path, file_name[:-4],
+                                                file_path, file_name[:-4]))
+        run('rm -rf {}{}/web_static'.format(file_path, file_name[:-4]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(file_path,
+                                                          file_name[:-4]))
         return True
-
+    except Exception:
+        return False
