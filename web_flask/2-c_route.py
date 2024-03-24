060990282f309adb087dcb541f2ef8bab4d45dbf
mazzinno <abdennasr3@gmail.com>
2024-03-24 03:26:15 +0000

    test

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB!"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnbtext(text):
+    """returns HBNB!"""
+    processed_text = text.replace('_', ' ')
+    return f'C {processed_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
