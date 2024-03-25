881574056657257bfa64dea849337635f9497a0f
mazzinno <abdennasr3@gmail.com>
2024-03-25 15:24:22 +0000

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
+"""the flask model"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    '''
-    Lists states from db
-    '''
-    state_dict = storage.all('State')
-    state_list = []
-
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('7-states_list.html', state_list=state_list)
+storage.all()
 
 
 @app.teardown_appcontext
-def teardown_app(e):
-    '''
-    teardown app context
-    '''
+def teardown_data(self):
+    """refrech data"""
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """it returns all states in the database"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
