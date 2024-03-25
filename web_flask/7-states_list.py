2101a1fe0f6ea0034abb78d38355890624faccd9
mazzinno <abdennasr3@gmail.com>
2024-03-25 05:54:29 +0000

    done

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""creating flask model"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+storage.all()
+
+
+@app.teardown_appcontext
+def teardown_data(self):
+    """refreches the data"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """this return all states in the database"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
