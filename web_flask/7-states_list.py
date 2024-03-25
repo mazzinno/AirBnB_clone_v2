fa689cc603057ca29c82b5349bae16e2747c11dc
mazzinno <abdennasr3@gmail.com>
2024-03-25 16:44:34 +0000

    testing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""the flask model"""
+"""
+Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
 app = Flask(__name__)
-storage.all()
-
-
-@app.teardown_appcontext
-def teardown_data(self):
-    """refrech data"""
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """it returns all states in the database"""
-    states = storage.all(State)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.teardown_appcontext
+def teardown_db(exception):
+    """this will close the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
